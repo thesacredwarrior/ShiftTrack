90c6c902467699f56b6ff41ff1e52d0e9b16a51a
Life Break <79377228+thesacredwarrior@users.noreply.github.com>
2024-02-13 23:06:46 +0300

    Start new project

--- app/src/main/java/com/example/shifttrack/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/shifttrack/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shifttrack
+package com.example.shifttrack.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shifttrack.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
